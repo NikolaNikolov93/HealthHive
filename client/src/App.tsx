3f4c76eca53aa516d13f6737992bb0aa9590f76b
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-12-23 23:33:22 +0200

    feat: added home/login/register routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,9 @@ import Header from "./components/header/Header";
 import Admin from "./pages/admin/Admin";
 import { Main } from "./App.styles";
 import AdminDashboard from "./pages/admin/AdminDashboard";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import LoginRegisterForm from "./components/form/LoginRegisterForm";
 
 function App() {
   return (
@@ -12,8 +15,12 @@ function App() {
         <Routes>
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginRegisterForm />} />
+          <Route path="/register" element={<LoginRegisterForm />} />
         </Routes>
       </Main>
+      <Footer />
     </>
   );
 }
