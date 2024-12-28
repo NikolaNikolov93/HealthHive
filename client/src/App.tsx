7e3958e1a48629ebc6daa1a3998616e3c6fc3207
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-12-28 12:27:06 +0200

    feat: setup Redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,22 +5,27 @@ import { Main } from "./App.styles";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
-import LoginRegisterForm from "./components/form/LoginRegisterForm";
+import { Provider } from "react-redux";
+import store from "./store";
+import LoginFormComponent from "./components/form/loginForm/LoginForm";
+import RegisterFormComponent from "./components/form/registerForm/RegisterForm";
 
 function App() {
   return (
     <>
-      <Header />
-      <Main>
-        <Routes>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginRegisterForm />} />
-          <Route path="/register" element={<LoginRegisterForm />} />
-        </Routes>
-      </Main>
-      <Footer />
+      <Provider store={store}>
+        <Header />
+        <Main>
+          <Routes>
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginFormComponent />} />
+            <Route path="/register" element={<RegisterFormComponent />} />
+          </Routes>
+        </Main>
+        <Footer />
+      </Provider>
     </>
   );
 }
