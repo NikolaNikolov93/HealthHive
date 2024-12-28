b75b85c19d065304cde05f689704f512cf2e5a89
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-12-28 12:27:51 +0200

    style: fixed ErrorSection text align

--- client/src/components/errorSection/ErrorSection.tsx
+++ client/src/components/errorSection/ErrorSection.tsx
@@ -1,35 +1,5 @@
-import styled from "styled-components";
-
-const ValidationList = styled.ul<{ $show: string }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  list-style: none;
-  padding: ${(props) => (props.$show === "true" ? "1em" : "0")};
-  margin: ${(props) => (props.$show === "true" ? "1em 0" : "0")};
-  max-height: ${(props) =>
-    props.$show === "true" ? "300px" : "0"}; /* Adjust max height as needed */
-  transition: max-height 0.3s ease, padding 0.3s ease, margin 0.3s ease;
-`;
-
-const ValidationMessage = styled.li<{ $isFixed: boolean }>`
-  display: flex;
-  align-items: center;
-  font-size: 0.9em;
-  color: ${(props) => (props.$isFixed ? "green" : "red")};
-  margin: 0.5em 0;
-  text-align: center;
-
-  &::after {
-    content: "${(props) => (props.$isFixed ? "✓" : "✗")}";
-    margin-left: 0.5em;
-    font-size: 1em;
-  }
-`;
-
-interface ErrorSectionProps {
-  errors: { message: string; isFixed: boolean }[];
-}
+import { ErrorSectionProps } from "./types";
+import { ValidationList, ValidationMessage } from "./ErrorSection.styles";
 
 const ErrorSection: React.FC<ErrorSectionProps> = ({ errors }) => {
   return (
